d9269fb2d344a325bc0224c30bbf8b2db6b77832
davidprates <davidjr.com@hotmail.com>
2020-03-13 12:16:22 +0000

    Mission complete

---
+++ js/js.js
@@ -0,0 +1,14 @@
+
+var missao = "Mission in execution"
+
+function eventOnload(){
+    console.log("Event on load triged");
+
+}
+
+function eventonclick() {
+    console.log("Mission Sucessful");
+}
+
+
+
